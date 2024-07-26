bc80d5338acea49a01e6029300a5d2127d3be3e1
Carlos Perez <pcarlos132@yahoo.com>
2024-07-25 19:53:48 -0500

    header and code structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,20 @@
-import "@/styles/globals.css";
+import "@/app/globals.css";
 import { Inter as FontSans } from "next/font/google";
 
 import { cn } from "@/lib/utils";
+import { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
+
+export const metadata: Metadata = {
+  title: "Live Docs",
+  description: "Your go-to collaborative editor",
+};
 
 export default function RootLayout({
   children,
@@ -14,16 +22,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head />
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
-      >
-        ...
-      </body>
-    </html>
+    <ClerkProvider
+    appearance={{
+      baseTheme: dark,
+      variables: {colorPrimary: "#3371FF", fontSize: "16px"},
+    }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={cn(
+            "min-h-screen font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
