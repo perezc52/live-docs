f6c8d438204cf073a3b0b668a4e1ad62af028794
Carlos Perez <pcarlos132@yahoo.com>
2024-07-31 20:02:52 -0500

    collaborative room functionality

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -6,10 +6,17 @@ import {
   ClientSideSuspense,
 } from "@liveblocks/react/suspense";
 import Loader from "@/components/Loader";
+import { getClerkUsers } from "@/lib/actions/users.actions";
 
 export default function Room({ children }: { children: ReactNode }) {
   return (
-    <LiveblocksProvider authEndpoint="/api/liveblocks-auth" resolveUsers={async ({userIds}) => {}}>
+    <LiveblocksProvider
+      authEndpoint="/api/liveblocks-auth"
+      resolveUsers={async ({ userIds }) => {
+        const users = await getClerkUsers({ userIds });
+        return users;
+      }}
+    >
       <ClientSideSuspense fallback={<Loader />}>{children}</ClientSideSuspense>
     </LiveblocksProvider>
   );
--- lib/actions/users.actions.ts
+++ lib/actions/users.actions.ts
@@ -1,6 +1,8 @@
 "use server";
 
 import { clerkClient } from "@clerk/nextjs/server";
+import { parseStringify } from "../utils";
+import { liveblocks } from "../liveblocks";
 
 export const getClerkUsers = async ({ userIds }: { userIds: string[] }) => {
   try {
@@ -14,7 +16,37 @@ export const getClerkUsers = async ({ userIds }: { userIds: string[] }) => {
       email: user.emailAddresses[0].emailAddress,
       avatar: user.imageUrl,
     }));
+
+    const sortedUsers = userIds.map((email) =>
+      users.find((user) => user.email === email)
+    );
+
+    return parseStringify(sortedUsers);
   } catch (error) {
     console.log(`error fetching users: ${error}`);
+  }
+};
+
+export const getDocument = async ({
+  roomId,
+  userId,
+}: {
+  roomId: string;
+  userId: string;
+}) => {
+  try {
+    const room = await liveblocks.getRoom(roomId);
+
+    //TODO: Bring back when we have the permissions system in place
+    // const hasAccess = Object.keys(room.usersAccesses).includes(userId);
+
+    // if (!hasAccess) {
+    //   throw new Error("You do not have access to this document");
+    // }
+
+    return parseStringify(room);
+
+  } catch (error) {
+    console.log(`Error happened while getting a room: ${error}`);
   }
 };
