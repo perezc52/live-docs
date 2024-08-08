e3f366c6072769285f26f95874e2e6984d4425d3
Carlos Perez <pcarlos132@yahoo.com>
2024-08-07 20:19:41 -0500

    comments

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -7,14 +7,25 @@ import {
 } from "@liveblocks/react/suspense";
 import Loader from "@/components/Loader";
 import { getClerkUsers } from "@/lib/actions/users.actions";
+import { currentUser } from "@clerk/nextjs/server";
+import { useUser } from "@clerk/nextjs";
 
 export default function Room({ children }: { children: ReactNode }) {
+  const { user: clerkUser } = useUser();
   return (
     <LiveblocksProvider
       authEndpoint="/api/liveblocks-auth"
       resolveUsers={async ({ userIds }) => {
         const users = await getClerkUsers({ userIds });
         return users;
+      }}
+      resolveMentionSuggestions={async ({ text, roomId }) => {
+        const roomUsers = await getDocumentUsers({
+          roomId,
+          currentUser: clerkUser?.emailAddresses[0].emailAddress,
+          text,
+        });
+        return roomUsers;
       }}
     >
       <ClientSideSuspense fallback={<Loader />}>{children}</ClientSideSuspense>
